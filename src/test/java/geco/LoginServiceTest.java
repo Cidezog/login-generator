e1e06f5fddb993f82fa5a242de64e909368695a2
Cidezog <enzo09100@gmail.com>
2016-11-22 11:25:05 +0100

    #1login-generator-1 Couvrir LoginService à 100 %

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,46 @@
+package geco;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class LoginServiceTest {
+
+    LoginService loginService;
+
+    @Before
+    public void setUp() throws Exception {
+        loginService = new LoginService(new String[]{"test1", "test2"});
+    }
+
+    @Test
+    public void testLoginExists() throws Exception {
+        Assert.assertTrue(loginService.loginExists("test1"));
+        Assert.assertFalse(loginService.loginExists("test3"));
+    }
+
+    @Test
+    public void testAddLogin() throws Exception {
+        Assert.assertFalse(loginService.loginExists("test3"));
+        loginService.addLogin("test3");
+        Assert.assertTrue(loginService.loginExists("test3"));
+    }
+
+    @Test
+    public void testFindAllLoginsStartingWith() throws Exception {
+        ArrayList<String> l = new ArrayList<String>();
+        l.add("test1");
+        l.add("test2");
+        Assert.assertEquals(l,loginService.findAllLoginsStartingWith("tes"));
+    }
+
+    @Test
+    public void testFindAllLogins() throws Exception {
+        ArrayList<String> l = new ArrayList<String>();
+        l.add("test1");
+        l.add("test2");
+        Assert.assertEquals(l,loginService.findAllLogins());
+    }
+}
